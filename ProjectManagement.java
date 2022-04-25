57feb4229791a100bfee1a8cd27ea320323012ad
Robin Singh <Robin.Singh1@wdc.com>
2022-04-25 19:44:46 +0530

    Adding complete workflow for classes

--- ProjectManagement.java
+++ ProjectManagement.java
@@ -1,42 +1,124 @@
 import java.time.Duration;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
 public class ProjectManagement {
 
+  //a default roadmap for the project. It defines the ratio of time required for various stages in the project.
+  final Map<ProjectStage, Integer> stageDurationRatio = Map.of(
+    ProjectStage.REQUIREMENT_GATHERING, 2,
+    ProjectStage.PROJECT_PLANNING, 2,
+    ProjectStage.EXECUTION, 4,
+    ProjectStage.ACCEPTANCE_TESTING, 1,
+    ProjectStage.RELEASE, 1
+    );
+
+
   /**
-   * evaluate equivalent effort from all resources, and calculate expected date of completionm for a project
+   * based on provided resources and dependencies calculate estimated completion
    * @param project object
    * @return date of completion for the project
   */
-  public Date getEstimatedCompletion(Project project) {
-    //
+  public LocalDate getEstimatedCompletion(Project project) {
+    //Compares a project's allocated resource to planned resource consumption.
+    //eg planned number of days worth of effort from human resource vs available human resource effort.
+
     return null;
+  }
+  
+  //Check if project can be completed within provided date.
+  public boolean checkProjectCompletion(Project project, LocalDate endDate) {
+    LocalDate estimatedCompletionDate = this.getEstimatedCompletion(project);
+
+    if (estimatedCompletionDate != null)
+      return estimatedCompletionDate.isAfter(endDate);
+    else
+      return false;
   }
 
   public static void main(String[] args) {
     System.out.println("Initializing Project Management Application");
+
+    HumanResource seniorDevelopers = new HumanResource();
+    seniorDevelopers.count = 6;
+    seniorDevelopers.experienceLevel = ExperienceLevel.SENIOR;
+    seniorDevelopers.role = Role.DEVELOPER;
+
+    HumanResource leadDevelopers = new HumanResource();
+    leadDevelopers.count = 2;
+    leadDevelopers.experienceLevel = ExperienceLevel.LEAD;
+    leadDevelopers.role = Role.DEVELOPER;
+
+    HumanResource projectManagers = new HumanResource();
+    projectManagers.count = 1;
+    projectManagers.experienceLevel = ExperienceLevel.MANAGEMENT;
+    leadDevelopers.role = Role.MANAGER;
+
+    Task projectDevelopmentTask = new Task();
+    projectDevelopmentTask.resources = List.of(seniorDevelopers, leadDevelopers, projectManagers);
+
+    LocalDate projectStartDate = LocalDate.now();
+
+    Project project = new Project();
+    project.startDate = projectStartDate;
+    project.timeline = Map.of(
+      ProjectStage.REQUIREMENT_GATHERING, projectStartDate.plusWeeks(2),
+      ProjectStage.PROJECT_PLANNING, projectStartDate.plusWeeks(4),
+      ProjectStage.EXECUTION, projectStartDate.plusMonths(2)
+    );
+    project.requiredHumanResourseUnits = 100;
+
+    ProjectManagement projectManagementApplication = new ProjectManagement();
+    LocalDate plannedCompletionDate = projectStartDate.plusMonths(5);
+
+    System.out.println(projectManagementApplication.checkProjectCompletion(project, plannedCompletionDate));
+    
   }
 }
 
 // Class to represent a project
 class Project {
   String name;
-  Date startDate;
-  Duration hoursRequired;
-  List<Task> dependencies;
-  Map<Stage, Date> timeline;
+  LocalDate startDate;
+  List<Task> tasks;
+  Map<ProjectStage, LocalDate> timeline;
 
+  //This is decided during project planning. Effort represented as number of days.
+  int requiredHumanResourseUnits;
 }
 
 // Class to represent different types of actors in project
-class Member {
-  int id;
-  String name;
+abstract class Resource {
+  String description;
+}
+
+class HumanResource extends Resource {
   Role role;
   ExperienceLevel experienceLevel;
+  int count;
 
+  //This assumes that on increasing level of expertise, the work output increases.
+  int getEffort() {
+    switch (experienceLevel) {
+      case LEAD:
+        return count*4;
+      case SENIOR: 
+        return count*2;
+      default:
+        return count*1;
+    }
+  }
+}
+
+class MaterialResource extends Resource {
+  String type;
+  int quantity;
+}
+
+class FinancialResource extends Resource {
+  double amount;
+  String description;
 }
 
 // Represents a unit of task.
@@ -44,15 +126,20 @@ class Task {
   String name;
   String description;
   Duration requiredTimeForCompletion;
-  Task dependency;
+  List<Resource> resources;
+  List<Task> externalDependency;
 
+  // Task(String name, String description, Duration duration, List<Resource> resources, List<Task> dependencies) {
+  //   this.name = name;
+
+  // }
 }
 
 enum Role {MANAGER, CONSULTANT, DEVELOPER}
 
-enum ExperienceLevel {JUNIOR, SENIOR, PRINCIPLE, MANAGEMENT}
+enum ExperienceLevel {JUNIOR, SENIOR, LEAD, MANAGEMENT}
 
-enum Stage {
-  REQUIREMENT_GATHERING, PROJECT_PLANNING, EXECUTION, ACCEPTANCE_TESTING, RELEASE  
+enum ProjectStage {
+  REQUIREMENT_GATHERING, PROJECT_PLANNING, EXECUTION, ACCEPTANCE_TESTING, RELEASE 
 }
 
--- readme.md
+++ readme.md
@@ -4,10 +4,20 @@ This project represents class structures to support a Project management applica
 
 # Features
 1. Represent a project using its attributes, resources and dependencies.
-2. Check estimated completion scenario for a project and available resources.
+2. Check estimated completion scenario for a project for given set of resources.
 
 # Models
-User
-Project
-Resource
-Task
+CLASSES:
+  Project
+  Resource
+  Task
+ENUM:
+
+
+# Use Cases/Assumptions
+1. Different type of members contribute work hours into the project.
+2. A project goes through set of stages.
+3. Each stage requires set of contributions from members of multiple skills.
+4. A project can depend on set of Tasks.
+5. A Task uses a set of Resources
+
